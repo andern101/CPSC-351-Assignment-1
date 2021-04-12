2ac80bcdfba7ccd93889527a4799add80a1d96e2
andern101 <50524760+andern101@users.noreply.github.com>
2021-04-12 16:18:02 -0700

    Update recv.cpp

--- recv.cpp
+++ recv.cpp
@@ -165,6 +165,6 @@ int main(int argc, char** argv)
 	mainLoop();
 
 	/** TODO: Detach from shared memory segment, and deallocate shared memory and message queue (i.e. call cleanup) **/
-		
+	cleanUp(shmid, msqid, sharedMemPtr);	
 	return 0;
 }
